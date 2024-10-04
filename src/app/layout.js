e20db082c648864aa833977f3738244dc603cad6
developermithu <developermithu@gmail.com>
2024-10-04 07:49:15 +0600

    added vercel geist font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,23 +1,13 @@
-import localFont from 'next/font/local'
+import { GeistSans } from 'geist/font/sans'
+
 import '@/app/global.css'
 import Header from '@/components/Header'
 
-const geistSans = localFont({
-    src: './fonts/GeistVF.woff',
-    variable: '--font-geist-sans',
-    weight: '100 900',
-})
-const geistMono = localFont({
-    src: './fonts/GeistMonoVF.woff',
-    variable: '--font-geist-mono',
-    weight: '100 900',
-})
-
 const RootLayout = ({ children }) => {
     return (
-        <html lang="en">
+        <html lang="en" className={GeistSans.className}>
             <body
-                className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#1a1f2e] text-white min-h-screen font-sans`}>
+                className={`antialiased bg-[#1a1f2e] text-white min-h-screen font-sans`}>
                 <Header />
                 {children}
             </body>
