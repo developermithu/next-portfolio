5c1b7155fd847fbdca44e5709c004168cbb6d983
developermithu <developermithu@gmail.com>
2024-10-28 23:26:06 +0600

    fixed nested routes request issue

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 const nextConfig = {
     async rewrites() {
         return [
+            // For nested routes /blog/posts
+            {
+                source: '/blog/posts/backend/:path*',
+                destination: process.env.NEXT_PUBLIC_BACKEND_URL + '/:path*',
+            },
             {
                 source: '/backend/:path*',
                 destination: process.env.NEXT_PUBLIC_BACKEND_URL + '/:path*',
